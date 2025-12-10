82ae40b39805dc243356107e4364dd463ba84043
Linneeeeeeee <lindsay.gelle@gmail.com>
2025-12-11 00:27:35 +1100

    Delete src/main/kotlin/AttributeAspect*.kt

--- src/main/kotlin/AttributeAspectContext.kt
+++
@@ -1,7 +0,0 @@
-package com.github.lindsaygelle
-
-class AttributeAspectContext(
-    override var aspect: Attribute,
-    override var aspectValue: Int,
-    override var value: Int
-) : AspectContext<Attribute, Int> {}
