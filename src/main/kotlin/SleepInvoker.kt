1d6ad7ec7c6b2ecba4f1dbecb458aa750197ce3b
Linneeeeeeee <lindsay.gelle@gmail.com>
2025-12-17 17:43:44 +1100

    Update SleepInvoker.kt

--- src/main/kotlin/SleepInvoker.kt
+++ src/main/kotlin/SleepInvoker.kt
@@ -1,8 +1,4 @@
 package com.github.lindsaygelle
 
-interface SleepInvoker: MagicInvoker {
-    var sleepRequirementMaximum: Int
-    var sleepRequirementMinimum: Int
-    val sleepRequirementRange: IntRange
-        get() = (sleepRequirementMinimum..sleepRequirementMaximum)
-}
+
+interface SleepInvoker : MagicInvoker, SleepRequirer
