91c949ce25d2b281a123d5c1dca93aac9e37ea4d
Linneeeeeeee <lindsay.gelle@gmail.com>
2025-12-17 17:11:30 +1100

    Update HurtInvoker.kt

--- src/main/kotlin/HurtInvoker.kt
+++ src/main/kotlin/HurtInvoker.kt
@@ -1,10 +1,6 @@
 package com.github.lindsaygelle
 
-interface HurtInvoker: MagicInvoker {
-    val hurtRange: IntRange
-        get() = (hurtRangeMinimum..hurtRangeMaximum)
-    var hurtRangeMaximum: Int
-    var hurtRangeMinimum: Int
+interface HurtInvoker: HurtRanger, MagicInvoker {
     var hurtScale: Int
     var hurtShift: Int
 }
