f292657eee4fa4efd3cd9f0327fe1b6e353914b8
Linneeeeeeee <lindsay.gelle@gmail.com>
2025-12-18 14:19:57 +1100

    Update ItemAbility.kt

--- src/main/kotlin/ItemAbility.kt
+++ src/main/kotlin/ItemAbility.kt
@@ -1,12 +1,14 @@
 package com.github.lindsaygelle
 
-abstract class ItemAbility<A : ItemInvoker, B : AbilityReceiver, C : ItemContext>(item: Item) : Ability<A, B, C>(),
+abstract class ItemAbility<A : ItemInvoker, B : AbilityReceiver, C : ItemContext>(item: Item, limit: Int) :
+    Ability<A, B, C>(limit),
     Itemizer {
     var item: Item = item
         set(value) {
             field = value
             logger.trace("item={}", field)
         }
+
     init {
         this.item = item
     }
