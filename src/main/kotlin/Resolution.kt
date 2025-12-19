97902de18f4568effc8afacd8eefbe9a02bff10e
Linneeeeeeee <lindsay.gelle@gmail.com>
2025-12-19 13:09:02 +1100

    Update Resolution.kt

--- src/main/kotlin/Resolution.kt
+++ src/main/kotlin/Resolution.kt
@@ -1,19 +1,10 @@
 package com.github.lindsaygelle
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-open class Resolution(resolved: Boolean) : TimeMeasurer {
-    protected val logger: Logger = LoggerFactory.getLogger(this::class.simpleName)
+open class Resolution(resolved: Boolean) : Context() {
     var resolved: Boolean = resolved
         set(value) {
             field = value
             logger.trace("resolved={}", field)
-        }
-    override var timeMilliseconds: Long = 0L
-        set(value) {
-            field = value
-            logger.trace("timeMilliseconds={}", field)
         }
 
     init {
