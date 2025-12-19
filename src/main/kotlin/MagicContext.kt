639017311e3b1babd7070ee74fffdebdc32af76b
Linneeeeeeee <lindsay.gelle@gmail.com>
2025-12-19 13:37:01 +1100

    Update MagicContext.kt

--- src/main/kotlin/MagicContext.kt
+++ src/main/kotlin/MagicContext.kt
@@ -1,4 +1,6 @@
 package com.github.lindsaygelle
 
-interface MagicContext : AbilityContext,
-    MagicPointer
+open class MagicContext(magicPoints: Int) : AbilityContext(),
+    MagicPointer {
+    override var magicPoints: Int = magicPoints
+}
