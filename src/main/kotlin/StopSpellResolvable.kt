fed112e5fa212885ff5ddccd8b8eaec6d1d2b2bd
Linneeeeeeee <lindsay.gelle@gmail.com>
2025-12-19 12:34:03 +1100

    Update StopSpellResolvable.kt

--- src/main/kotlin/StopSpellResolvable.kt
+++ src/main/kotlin/StopSpellResolvable.kt
@@ -1,7 +1,13 @@
 package com.github.lindsaygelle
 
-class StopSpellResolvable: Resolvable<StopSpellResolver> {
-    override fun resolve(resolver: StopSpellResolver): Resolution {
-        return StopSpellResolution()
+class StopSpellResolvable(resolver: StopSpellResolver) : Resolvable<StopSpellResolver, StopSpellResolution>(resolver) {
+    override fun resolve(): StopSpellResolution {
+        val stopSpellResolutionRandom = resolver.stopSpellResolutionRandom
+        val resolved = stopSpellResolutionRandom == resolver.stopSpellResolutionMaximum
+        if (resolved) {
+            resolver.statusStopSpell = false
+            resolver.turnsStopSpell = 0
+        }
+        return StopSpellResolution(resolved, stopSpellResolutionRandom)
     }
 }
