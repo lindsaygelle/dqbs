c1611eb817aeb1be327705aa1c90e15c3f533ae4
Linneeeeeeee <lindsay.gelle@gmail.com>
2025-12-19 12:30:28 +1100

    Update StopSpellResolution.kt

--- src/main/kotlin/StopSpellResolution.kt
+++ src/main/kotlin/StopSpellResolution.kt
@@ -1,3 +1,13 @@
 package com.github.lindsaygelle
 
-class StopSpellResolution : Resolution(System.currentTimeMillis())
+class StopSpellResolution(resolved: Boolean, stopSpellResolutionRandom: Int) : Resolution(resolved) {
+    var stopSpellResolutionRandom: Int = stopSpellResolutionRandom
+        set(value) {
+            field = value
+            logger.trace("stopSpellResolutionRandom={}", field)
+        }
+
+    init {
+        this.stopSpellResolutionRandom = stopSpellResolutionRandom
+    }
+}
