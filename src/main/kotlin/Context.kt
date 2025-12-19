acf0e6e814d3f29e3898f266742162201e500db8
Linneeeeeeee <lindsay.gelle@gmail.com>
2025-12-19 13:08:14 +1100

    Update Context.kt

--- src/main/kotlin/Context.kt
+++ src/main/kotlin/Context.kt
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory
 
 open class Context(timeMilliseconds: Long = System.currentTimeMillis()) : TimeMeasurer {
     protected val logger: Logger = LoggerFactory.getLogger(this::class.simpleName)
-    override var timeMilliseconds: Long = timeMilliseconds
+    final override var timeMilliseconds: Long = timeMilliseconds
         set(value) {
             field = maxOf(0, value)
             logger.trace("timeMilliseconds={}", field)
