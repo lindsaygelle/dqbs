f4a3d24aed04ac8acf385a1f4230f790128a0a0b
Linneeeeeeee <lindsay.gelle@gmail.com>
2025-12-19 13:15:59 +1100

    Update BattleContext.kt

--- src/main/kotlin/BattleContext.kt
+++ src/main/kotlin/BattleContext.kt
@@ -1,26 +1,15 @@
 package com.github.lindsaygelle
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
 class BattleContext(
-    timeMilliseconds: Long,
     turnContext: TurnContext,
     turns: Int
-) : Context, TurnsAccumulator {
+) : Context(), TurnsAccumulator {
     var turnContext: TurnContext = turnContext
         set(value) {
             field = value
             logger.trace("turnContext={}", field)
         }
 
-    @Transient
-    private val logger: Logger = LoggerFactory.getLogger(this::class.simpleName)
-    override var timeMilliseconds: Long = timeMilliseconds
-        set(value) {
-            field = maxOf(0L, value)
-            logger.trace("timeMilliseconds={}", field)
-        }
     override var turns: Int = turns
         set(value) {
             field = maxOf(0, value)
@@ -28,7 +17,6 @@ class BattleContext(
         }
 
     init {
-        this.timeMilliseconds = timeMilliseconds
         this.turnContext = turnContext
         this.turns = turns
     }
