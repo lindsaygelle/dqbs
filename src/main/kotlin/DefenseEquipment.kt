70bfe535b73ef1059091976af42a6aec89d4baff
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-06 11:52:16 +1100

    Create DefenseEquipment.kt

---
+++ src/main/kotlin/DefenseEquipment.kt
@@ -0,0 +1,20 @@
+package com.github.lindsaygelle
+
+import java.util.UUID
+
+open class DefenseEquipment(defense: Int, name: String, uuid: UUID) : DefensePointer,
+    Equipment(name, uuid) {
+    final override var defense: Int = defense
+        set(value) {
+            field = maxOf(0, value)
+            logger.trace("defense={}", field)
+        }
+
+    init {
+        this.defense = defense
+    }
+
+    override fun toString(): String {
+        return "defense=${defense} ${super.toString()}"
+    }
+}
