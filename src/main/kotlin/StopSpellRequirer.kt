811642e243bedbce1bfb55e6af8687b55d887fae
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-06 14:44:16 +1100

    Update StopSpellRequirer.kt

--- src/main/kotlin/StopSpellRequirer.kt
+++ src/main/kotlin/StopSpellRequirer.kt
@@ -1,8 +1,6 @@
 package com.github.lindsaygelle
 
 interface StopSpellRequirer {
-    val stopSpellRequirementRandom: Int
-        get() = stopSpellRequirementRange.random()
     var stopSpellRequirementMaximum: Int
     var stopSpellRequirementMinimum: Int
     val stopSpellRequirementRange: IntRange
