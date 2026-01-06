429bd32cea6f3bfe93576a7812036b5dad267b6c
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-06 11:56:28 +1100

    Update HealRanger.kt

--- src/main/kotlin/HealRanger.kt
+++ src/main/kotlin/HealRanger.kt
@@ -1,10 +1,10 @@
 package com.github.lindsaygelle
 
 interface HealRanger {
-    val healRandom: Int
-        get() = healRange.random()
     val healRange: IntRange
         get() = (healRangeMinimum..healRangeMaximum)
+    val healRangeRandom: Int
+        get() = healRange.random()
     var healRangeMaximum: Int
     var healRangeMinimum: Int
 }
