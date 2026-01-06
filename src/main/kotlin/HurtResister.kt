57b9ba3529600e1b5dbbc58b77ca5a1a302243a2
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-06 13:15:58 +1100

    Update HurtResister.kt

--- src/main/kotlin/HurtResister.kt
+++ src/main/kotlin/HurtResister.kt
@@ -3,8 +3,8 @@ package com.github.lindsaygelle
 interface HurtResister {
     var hurtResistanceMaximum: Int
     var hurtResistanceMinimum: Int
-    val hurtResistanceRandom: Int
-        get() = hurtResistanceRange.random()
     val hurtResistanceRange: IntRange
         get() = (hurtResistanceMinimum..hurtResistanceMaximum)
+    val hurtResistanceRangeRandom: Int
+        get() = hurtResistanceRange.random()
 }
