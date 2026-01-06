f95b7173ae9aebbf74f96b12d10980f206d98fc2
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-06 14:34:50 +1100

    Update SleepRequirer.kt

--- src/main/kotlin/SleepRequirer.kt
+++ src/main/kotlin/SleepRequirer.kt
@@ -3,8 +3,8 @@ package com.github.lindsaygelle
 interface SleepRequirer {
     var sleepRequirementMaximum: Int
     var sleepRequirementMinimum: Int
-    val sleepRequirementRandom: Int
-        get() = sleepRequirementRange.random()
     val sleepRequirementRange: IntRange
         get() = (sleepRequirementMinimum..sleepRequirementMaximum)
+    val sleepRequirementRangeRandom: Int
+        get() = sleepRequirementRange.random()
 }
