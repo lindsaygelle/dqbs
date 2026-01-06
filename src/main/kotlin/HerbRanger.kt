1b5c0e8b48e3383d951f8fe0266d8bb2c76f98da
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-06 14:37:37 +1100

    Update HerbRanger.kt

--- src/main/kotlin/HerbRanger.kt
+++ src/main/kotlin/HerbRanger.kt
@@ -3,8 +3,6 @@ package com.github.lindsaygelle
 interface HerbRanger {
     val herbRange: IntRange
         get() = (herbRangeMinimum..herbRangeMaximum)
-    val herbRangeRandom: Int
-        get() = herbRange.random()
     var herbRangeMaximum: Int
     var herbRangeMinimum: Int
 }
