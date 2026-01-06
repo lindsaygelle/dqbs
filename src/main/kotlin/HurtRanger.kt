7f1f99d2b227f89a4fa67a3d1ca25a7b34d9dc27
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-06 14:38:04 +1100

    Update HurtRanger.kt

--- src/main/kotlin/HurtRanger.kt
+++ src/main/kotlin/HurtRanger.kt
@@ -3,8 +3,6 @@ package com.github.lindsaygelle
 interface HurtRanger {
     val hurtRange: IntRange
         get() = (hurtRangeMinimum..hurtRangeMaximum)
-    val hurtRangeRandom: Int
-        get() = hurtRange.random()
     var hurtRangeMaximum: Int
     var hurtRangeMinimum: Int
 }
