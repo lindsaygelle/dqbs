666ceba21fc1dbd095e88e02d95d41e74b009956
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-06 17:18:27 +1100

    Update SleepReception.kt

--- src/main/kotlin/SleepReception.kt
+++ src/main/kotlin/SleepReception.kt
@@ -2,8 +2,20 @@ package com.github.lindsaygelle
 
 class SleepReception(sleepResistance: Int, sleepResistanceMaximum: Int, sleepResistanceMinimum: Int) : Reception() {
     var sleepResistance: Int = sleepResistance
+        set(value) {
+            field = value
+            logger.trace("sleepResistance={}", field)
+        }
     var sleepResistanceMaximum: Int = sleepResistanceMaximum
+        set(value) {
+            field = value
+            logger.trace("sleepResistanceMaximum={}", field)
+        }
     var sleepResistanceMinimum: Int = sleepResistanceMinimum
+        set(value) {
+            field = value
+            logger.trace("sleepResistanceMinimum={}", field)
+        }
 
     init {
         this.sleepResistance = sleepResistance
