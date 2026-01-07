7b05229319b4904198f69ee02eb69036b41ffa05
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-07 08:45:47 +0000

    Dev

--- src/main/kotlin/Heal.kt
+++ src/main/kotlin/Heal.kt
@@ -3,8 +3,7 @@ package com.github.lindsaygelle
 import java.util.*
 
 class Heal(
-    limit: Int,
-    magicPoints: Int
+    limit: Int, magicPoints: Int
 ) : MagicAbility<HealInvoker, HealReceiver, HealInvocation, HealReception, HealCheck, HealEffect>(limit, magicPoints) {
     override fun getCheckable(): Checkable<HealInvocation, HealReception, HealCheck> {
         return HealCheckable(magicPoints)
@@ -20,7 +19,7 @@ class Heal(
         if (check.result) {
             receiver.hitPoints += invocation.heal
         }
-        return HealEffect(invocation.heal, System.currentTimeMillis(), UUID.randomUUID())
+        return HealEffect(receiver.hitPoints, reception.hitPoints, System.currentTimeMillis(), UUID.randomUUID())
     }
 
     override fun getInvocable(): Invocable<HealInvoker, HealInvocation> {
--- src/main/kotlin/HealEffect.kt
+++ src/main/kotlin/HealEffect.kt
@@ -2,19 +2,28 @@ package com.github.lindsaygelle
 
 import java.util.*
 
-class HealEffect(hitPoints: Int, timeMilliseconds: Long, uuid: UUID) : Effect(timeMilliseconds, uuid),
+class HealEffect(hitPoints: Int, hitPointsPrevious: Int, timeMilliseconds: Long, uuid: UUID) : Effect(
+    timeMilliseconds,
+    uuid
+),
     HitPointer {
     override var hitPoints: Int = hitPoints
         set(value) {
             field = value
             logger.trace("hitPoints={}", field)
         }
+    var hitPointsPrevious: Int = hitPointsPrevious
+        set(value) {
+            field = value
+            logger.trace("hitPointsPrevious={}", field)
+        }
 
     init {
         this.hitPoints = hitPoints
+        this.hitPointsPrevious = hitPointsPrevious
     }
 
     override fun toString(): String {
-        return "{hitPoints=${hitPoints} ${super.toString()}}"
+        return "{hitPoints=${hitPoints} hitPointsPrevious=${hitPointsPrevious} ${super.toString()}}"
     }
 }
