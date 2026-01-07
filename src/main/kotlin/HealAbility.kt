554fdf8a59f9824df02b607e4c85473a81543303
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-07 23:41:44 +0000

    Dev

--- src/main/kotlin/HealAbility.kt
+++ src/main/kotlin/HealAbility.kt
@@ -1,12 +1,12 @@
 package com.github.lindsaygelle
 
-abstract class HealAbility<A: MagicInvoker>(
+abstract class HealAbility<A>(
     limit: Int,
     magicPoints: Int
 ) : MagicAbility<A, HealReceiver, HealInvocation, HealReception, HealCheck, HealEffect>(
     limit,
     magicPoints
-) {
+) where A : MagicInvoker, A : HealRanger {
     final override fun getCheckable(): Checkable<HealInvocation, HealReception, HealCheck> {
         return HealCheckable(magicPoints)
     }
