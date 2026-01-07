00ed900c954d9328057c5bec33b0705c1f40b16a
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-07 02:12:24 +0000

    Dev

--- src/main/kotlin/Effect.kt
+++ src/main/kotlin/Effect.kt
@@ -1,3 +1,29 @@
 package com.github.lindsaygelle
 
-open class Effect
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.util.*
+
+open class Effect(timeMilliseconds: Long, uuid: UUID) : TimeMeasurer, UniversalIdentifier {
+    @Transient
+    protected val logger: Logger = LoggerFactory.getLogger(this::class.simpleName)
+    final override var timeMilliseconds: Long = timeMilliseconds
+        set(value) {
+            field = value
+            logger.trace("timeMilliseconds={}", field)
+        }
+    final override var uuid: UUID = uuid
+        set(value) {
+            field = value
+            logger.trace("uuid={}", field)
+        }
+
+    init {
+        this.timeMilliseconds = timeMilliseconds
+        this.uuid = uuid
+    }
+
+    override fun toString(): String {
+        return "hashCode=${hashCode()} timeMilliseconds=${timeMilliseconds} uuid=${uuid}"
+    }
+}
