52cf6a96b9fa2e3eef3d115242f73a254c0cb406
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-07 02:06:45 +0000

    Dev

--- src/main/kotlin/SleepReceivable.kt
+++ src/main/kotlin/SleepReceivable.kt
@@ -1,9 +1,15 @@
 package com.github.lindsaygelle
 
+import java.util.*
+
 class SleepReceivable : Receivable<SleepReceiver, SleepReception>() {
     override fun receive(receiver: SleepReceiver): SleepReception {
         return SleepReception(
-            receiver.sleepResistance, receiver.sleepResistanceMaximum, receiver.sleepResistanceMinimum
+            receiver.sleepResistance,
+            receiver.sleepResistanceMaximum,
+            receiver.sleepResistanceMinimum,
+            System.currentTimeMillis(),
+            UUID.randomUUID()
         )
     }
 }
