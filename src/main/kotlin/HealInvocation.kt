dc25ef8372dff2f28b8e7521cd26259b3485d773
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-07 08:08:04 +0000

    Dev

--- src/main/kotlin/HealInvocation.kt
+++ src/main/kotlin/HealInvocation.kt
@@ -2,20 +2,54 @@ package com.github.lindsaygelle
 
 import java.util.*
 
-class HealInvocable(magicPoints: Int) : MagicInvocable<HealInvoker, HealInvocation>(magicPoints) {
-    override fun invoke(invoker: HealInvoker): HealInvocation {
-        val healRandom = invoker.healRange.random()
-        val healOffset = (healRandom and invoker.healShift) + invoker.healScale
-        val heal = maxOf(0, healOffset)
-        return HealInvocation(
-            heal,
-            healOffset,
-            healRandom,
-            invoker.healRangeMaximum,
-            invoker.healRangeMinimum,
-            invoker.magicPoints,
-            System.currentTimeMillis(),
-            UUID.randomUUID(),
-        )
+class HealInvocation(
+    heal: Int,
+    healOffset: Int,
+    healRandom: Int,
+    healRangeMaximum: Int,
+    healRangeMinimum: Int,
+    magicPoints: Int,
+    timeMilliseconds: Long,
+    uuid: UUID
+) : MagicInvocation(
+    magicPoints,
+    timeMilliseconds,
+    uuid
+) {
+    var heal: Int = heal
+        set(value) {
+            field = value
+            logger.trace("heal={}", field)
+        }
+    var healOffset: Int = healOffset
+        set(value) {
+            field = value
+            logger.trace("healOffset={}", field)
+        }
+    var healRandom: Int = healRandom
+        set(value) {
+            field = value
+            logger.trace("healRandom={}", field)
+        }
+    var healRangeMaximum: Int = healRangeMaximum
+        set(value) {
+            field = value
+            logger.trace("healRangeMaximum={}", field)
+        }
+    var healRangeMinimum: Int = healRangeMinimum
+        set(value) {
+            field = value
+            logger.trace("healRangeMinimum={}", field)
+        }
+
+    init {
+        this.heal = heal
+        this.healRandom = healRandom
+        this.healRangeMaximum = healRangeMaximum
+        this.healRangeMinimum = healRangeMinimum
+    }
+
+    override fun toString(): String {
+        return "{heal=${heal} healOffset=${healOffset} healRandom=${healRandom} healRangeMaximum=${healRangeMaximum} healRangeMinimum=${healRangeMinimum} ${super.toString()}}"
     }
 }
