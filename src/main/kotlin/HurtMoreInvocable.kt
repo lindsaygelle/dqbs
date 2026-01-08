18fddb7cc495f75fb92c38837d133ebbf53795e0
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-08 02:35:57 +0000

    Dev

---
+++ src/main/kotlin/HurtMoreInvocable.kt
@@ -0,0 +1,22 @@
+package com.github.lindsaygelle
+
+import java.util.UUID
+
+class HurtMoreInvocable : MagicInvocable<HurtMoreInvoker, HurtInvocation>() {
+    override fun invoke(invoker: HurtMoreInvoker): HurtInvocation {
+        val hurtRandom = invoker.hurtRange.random()
+        val hurtOffset = (hurtRandom and invoker.hurtMoreShift) + invoker.hurtMoreScale
+        val hurt = maxOf(0, hurtOffset)
+        return HurtInvocation(
+            hurt,
+            hurtOffset,
+            hurtRandom,
+            invoker.hurtRequirement,
+            invoker.hurtRequirementMaximum,
+            invoker.hurtRequirementMinimum,
+            invoker.magicPoints,
+            System.currentTimeMillis(),
+            UUID.randomUUID(),
+        )
+    }
+}
---
+++ src/test/kotlin/HurtMoreTest.kt
@@ -0,0 +1,26 @@
+import com.github.lindsaygelle.Armor
+import com.github.lindsaygelle.HurtMore
+import com.github.lindsaygelle.HurtReceiver
+import java.util.*
+import kotlin.random.Random
+import kotlin.test.Test
+
+class HurtMoreTest {
+    @Test
+    fun test() {
+        val limit = Random.nextInt(1, 10)
+        val magicPoints = Random.nextInt(0, 100)
+        val hurtMore = HurtMore(limit, magicPoints)
+        val invoker = MockHurtMoreInvoker(4, 1, 16, 0, 16, 1, Random.nextInt(0, magicPoints * 2), UUID.randomUUID())
+        val receivers = mutableListOf<HurtReceiver>()
+        for (i in 0..Random.nextInt(3, 100)) {
+            var armor: Armor? = null
+            if (listOf(true, false).random()) {
+                armor = Armor(Random.nextInt(0, 10), Random.nextInt(0, 10), "TEST", UUID.randomUUID())
+            }
+            receivers.add(MockHurtReceiver(armor, Random.nextInt(1, 100), 16, 0, UUID.randomUUID()))
+        }
+        val outcomes = hurtMore.use(invoker, receivers)
+        print(outcomes)
+    }
+}
