125d305d9d9d6504c32b09c7d6f2e5d8bcaaf80d
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-09 04:22:44 +0000

    Dev

--- src/main/kotlin/Assessment.kt
+++ src/main/kotlin/Assessment.kt
@@ -1,3 +1,42 @@
 package com.github.lindsaygelle
 
-class Assessment
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.util.*
+
+class Assessment(evaluations: List<Evaluation>, result: Boolean, timeMilliseconds: Long, uuid: UUID) : TimeMeasurer,
+    UniversalIdentifier {
+    var evaluations: List<Evaluation> = evaluations
+        set(value) {
+            field = value
+            logger.trace("evaluations={} evaluations.size={}", field, field.size)
+        }
+
+    @Transient
+    private val logger: Logger = LoggerFactory.getLogger(this::class.simpleName)
+
+    var result: Boolean = result
+        set(value) {
+            field = value
+            logger.trace("result={}", field)
+        }
+
+    override var timeMilliseconds: Long = timeMilliseconds
+        set(value) {
+            field = value
+            logger.trace("timeMilliseconds={}", field)
+        }
+
+    override var uuid: UUID = uuid
+        set(value) {
+            field = value
+            logger.trace("uuid={}", field)
+        }
+
+    init {
+        this.evaluations = evaluations
+        this.result = result
+        this.timeMilliseconds = timeMilliseconds
+        this.uuid = uuid
+    }
+}
