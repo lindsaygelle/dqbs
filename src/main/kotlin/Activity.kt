4d0fef8b4b30470d7a21fe30144acd9e92722e70
Linneeeeeeee <lindsay.gelle@gmail.com>
2026-01-12 01:04:10 +0000

    Dev

--- src/main/kotlin/Activity.kt
+++ src/main/kotlin/Activity.kt
@@ -13,10 +13,21 @@ class Activity(
             logger.trace("appraisal={}", field)
         }
 
+    var decision: Decision? = decision
+        set(value) {
+            field = value
+            logger.trace("decision={}", field)
+        }
+
     @Transient
     private val logger: Logger = LoggerFactory.getLogger(this::class.simpleName)
 
     init {
         this.appraisal = appraisal
+        this.decision = decision
+    }
+
+    override fun toString(): String {
+        return "{appraisal=${appraisal} decision=${decision} hashCode=${hashCode()}}"
     }
 }
